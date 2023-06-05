f552a55bbf725cf83fe502ccef7c324a2214ba25
keigobrand <keigobrand@gmail.com>
2023-06-05 07:37:27 +0200

    pruena cookies

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,17 @@ function animateBars() {
 }
 
 /*mantener modo socuro o claro */
-let modo=document.getElementById("switch");
-let body=document.body;
+let modo = document.getElementById("switch");
+let body = document.body;
 
-modo.addEventListener("click", function(){
-    let val=body.classList.toggle("dark")
-    localStorage.setItem("modo",val)
+modo.addEventListener("click", function () {
+    let val = body.classList.toggle("dark")
+    localStorage.setItem("modo", val)
 })
 
-let valor=localStorage.getItem("modo")
+let valor = localStorage.getItem("modo")
 
-if (valor=="true") {
+if (valor == "true") {
     body.classList.add("dark")
 } else {
     body.classList.remove("dark")
@@ -45,4 +45,46 @@ const preferenciaColor = window.matchMedia('(prefers-color-scheme: dark)').match
 
 if (localStorage.getItem("modo") === "true" || (localStorage.getItem("modo") === null && preferenciaColor)) {
     body.classList.add("dark");
+}
+
+
+/*cookies */
+
+window.addEventListener('load', function () {
+    var cookiePopup = document.getElementById('cookie-popup');
+    var acceptCookieButton = document.getElementById('accept-cookie');
+
+    if (!getCookie('cookie_accepted')) {
+        cookiePopup.style.display = 'block';
+    }
+
+    acceptCookieButton.addEventListener('click', function () {
+        setCookie('cookie_accepted', true, 365);
+        cookiePopup.style.display = 'none';
+    });
+});
+
+function setCookie(name, value, days) {
+    var expires = '';
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
+
+function getCookie(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
 }
